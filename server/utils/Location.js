dc462324a8ebb9b0b098743c6b9599d40511030a
iiTONELOC <anthonytropeano@me.com>
2021-07-19 15:38:38 -0400

    cleaning up code, adding error handling

--- server/utils/Location.js
+++ server/utils/Location.js
@@ -7,32 +7,33 @@ class Location {
 
         const client = req.ip
         if (client === '::1' || client === '::ffff:127.0.0.1') {
-            const response = await extIP.get().then(ip => {
-                const locationData = geoip.lookup(ip)
-                const lat = locationData.ll[0];
-                const lon = locationData.ll[1];
-                const city = locationData.city;
-                const state = locationData.region;
-                const data = {
-                    city: city,
-                    state: state,
-                    lat: lat,
-                    lon: lon,
-                }
-                return data
-            }, err => {
-                console.log(err);
-                return
-            }).catch(e => {
-                console.log(e)
-                return
-            })
-            return response;
+            try {
+                const response = await extIP.get().then(ip => {
+                    const locationData = geoip.lookup(ip)
+                    const lat = locationData.ll[0];
+                    const lon = locationData.ll[1];
+                    const { city, state } = locationData
+                    const data = {
+                        city: city,
+                        state: state,
+                        lat: lat,
+                        lon: lon,
+                    }
+                    return data
+                }, err => {
+                    console.log(err);
+                    return
+                })
+                return response;
+            } catch (error) {
+                console.error(error)
+            }
         } else {
             const ip = geoip.lookup(client);
+            const { city, state } = ip
             const data = {
-                city: ip.city,
-                state: ip.region,
+                city: city,
+                state: state,
                 lat: ip.ll[0],
                 lon: ip.ll[1],
             }
@@ -43,10 +44,14 @@ class Location {
     static async user(args, context) {
         const { latitude, longitude, } = args;
         if (latitude == undefined || longitude == undefined) {
-            const { lat, lon } = await Location.get(context);
-            return {
-                latitude: lat,
-                longitude: lon
+            try {
+                const { lat, lon } = await Location.get(context);
+                return {
+                    latitude: lat,
+                    longitude: lon
+                }
+            } catch (error) {
+                console.error("Error occurred while fetching location details", error)
             }
         } else {
             return {
