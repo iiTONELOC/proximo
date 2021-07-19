5f1630d1837d1f0cdd0302e8691f2e9b5daa72ae
iiTONELOC <anthonytropeano@me.com>
2021-07-19 15:08:50 -0400

    moved the chat server to its own separate module

--- chat-server/index.js
+++ chat-server/index.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require('../server/node_modules/express');
 const CHATPORT = process.env.CHATPORT || 8080
 const socketIo = require('socket.io');
 const cors = require('cors');
-const chatApp = express();
+const app = express();
 const NetAddress = require('./utils/network');
 const chat = require('./utils/chat-handler')
-const server = require('http').createServer(chatApp);
+const server = require('http').createServer(app);
 const io = socketIo(server, {
     cors: {
         origin: '*',
         methods: ['GET', 'POST']
     }
 })
-chatApp.use(cors())
+app.use(cors())
 
 const ChatServer = () => {
     server.listen(CHATPORT, () => {
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const db = require('./config/connection');
-const ChatServer = require('./chatServer');
+const ChatServer = require('../chat-server');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const requestIp = require('request-ip');
@@ -45,6 +45,6 @@ db.once('open', () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    ChatServer()
   });
-  ChatServer();
 });
