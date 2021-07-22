dd63380b14421808dada6ae5d8d07efa9865ec63
iiTONELOC <anthonytropeano@me.com>
2021-07-22 11:57:28 -0400

    removed logs from userinrange virtual

--- server/models/User.js
+++ server/models/User.js
@@ -71,32 +71,29 @@ userSchema.virtual('friendCount').get(function () {
   return this.friends.length;
 });
 
-userSchema.virtual('UsersInRange').get(async function(){
-// query all active users return an array of users within 2 miles for now, change distance later
-const Users = await User.find();
-// calculate distance between users
-const [data] = this.location
-const {latitude, longitude } = data;
-return Users.map((el)=>{
+userSchema.virtual('UsersInRange').get(async function () {
+  // query all active users return an array of users within 2 miles for now, change distance later
+  const Users = await User.find();
+  // calculate distance between users
+  const [data] = this.location
+  const { latitude, longitude } = data;
+  const userData = Users.map((el) => {
+    const [data2] = el.location;
+    const lat2 = data2.latitude;
+    const long2 = data2.longitude;
+    const tooFar = Distance(latitude, longitude, lat2, long2)
+    if (this._id.toString() === el._id.toString()) {
+      // no need to calculate distance on the the same user
+    } else {
+      // if not user, calculate current distance between this user and all others
+      if (tooFar < 2) {
+        return data._id
+      }
+    }
+  })
 
-const [data2] = el.location;
-const lat2 = data2.latitude;
-const long2 = data2.longitude;
-const tooFar = Distance(latitude, longitude, lat2, long2)
-if(this._id.toString() === el._id.toString()){
-  console.log('HERE')
-  console.log(tooFar)
-}else{
-  console.log(this._id)
-  console.log(el._id)
-  if(tooFar <=2){
-    
-    return data._id
-  }
-}
-
-
-})
+  // filter array to remove null values
+  return userData.filter(el=>el != null)
 })
 
 const User = model('User', userSchema);
