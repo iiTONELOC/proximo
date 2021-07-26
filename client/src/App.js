552c9c3e299ca16f3f6c96482516038de3f2cf96
ItsEvdok <semionaevdokimov@icloud.com>
2021-07-26 13:47:37 -0400

    updating npm

--- client/src/App.js
+++ client/src/App.js
@@ -40,6 +40,7 @@ function App() {
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/public" component={Public} />
+              <Route exact path="/dashboard" component={Dashboard} />
               {/* <Route exact path="/profile" component={Profile} /> */}
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
