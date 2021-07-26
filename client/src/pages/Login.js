ab267c93c20591cfb9a7208b8c52d72c447d75a1
iiTONELOC <anthonytropeano@me.com>
2021-07-26 13:50:56 -0400

    login/signup

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -14,7 +14,7 @@ const Login = (props) => {
       [name]: value,
     });
   };
-  
+
 
   // submit form
   const handleFormSubmit = async event => {
@@ -32,39 +32,77 @@ const Login = (props) => {
   };
 
   return (
-    <main className='flex-row justify-center mb-4'>
-      <div className='col-12 col-md-6'>
-        <div className='card'>
-          <h4 className='card-header'>Login</h4>
-          <div className='card-body'>
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className='form-input'
-                placeholder='Your email'
-                name='email'
-                type='email'
-                id='email'
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className='form-input'
-                placeholder='******'
-                name='password'
-                type='password'
-                id='password'
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className='btn d-block w-100' type='submit'>
-                Submit
-              </button>
-              {error && <div>Login failed</div>}
-            </form>
+    <div className="h-full bg-gray-800 flex sm:p-4">
+      <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+        <div className="mx-auto w-full max-w-sm lg:w-96">
+          <div>
+
+            <h2 className="mt-6 text-3xl font-extrabold text-gray-100">LOGIN</h2>
+            {error && <div>Sign up failed</div>}
+
+            <div className="mt-6">
+              <form onSubmit={handleFormSubmit}>
+
+
+                <div>
+                  <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                    Email
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      placeholder='Your email'
+                      name='email'
+                      type='email'
+                      id='email'
+                      value={formState.email}
+                      onChange={handleChange}
+                      required
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="space-y-1">
+                  <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                    Password
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      placeholder='******'
+                      name='password'
+                      type='password'
+                      id='password'
+                      value={formState.password}
+                      onChange={handleChange}
+                      required
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+
+
+                <div>
+                  <button
+                    type="submit"
+                    className="w-full mt-5 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Sign in
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </main>
+      <div className="hidden lg:block relative w-0 flex-1">
+        <img
+          className="absolute inset-0 h-full w-full object-cover"
+          src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+          alt=""
+        />
+      </div>
+    </div>
   );
 };
 
