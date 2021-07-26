8534ef7eb2502c4a0c98aed10438ec79feea8c93
iiTONELOC <anthonytropeano@me.com>
2021-07-26 17:46:35 -0400

    form validation on sign up

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { validateEmail } from '../utils/helpers'
+import { validateEmail } from '../utils/helpers';
+import createError from '../components/Alerts/Sign-up/createError'
 
 export default function Example() {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
   const [addUser, { error }] = useMutation(ADD_USER);
-  const [errorMessage, setErrorMessage] = useState([]);
+  const [userError, setUserError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [signUpError, setAlert] = useState('')
   // update state based on form input changes
 
   // useEffect(() => {
@@ -18,23 +22,22 @@ export default function Example() {
     setFormState({ ...formState, [e.target.name]: e.target.value });
     if (e.target.name === 'email') {
       const isValid = validateEmail(e.target.value);
-      console.log(isValid);
       if (!isValid) {
-        setErrorMessage({ email: 'Your email is invalid.' });
+        setEmailError('Your email is invalid.');
       } else {
-        setErrorMessage('');
+        setEmailError('');
       }
     } else if (e.target.name === 'username') {
       if (e.target.value.length < 2) {
-        setErrorMessage({ username: `Username's must be at least 2 characters!` })
+        setUserError(`Username's must be at least 2 characters!`)
       } else {
-        setErrorMessage({ username: `` })
+        setUserError('')
       }
-    } else {
-      if (e.target.value.length < 5) {
-        setErrorMessage({ password: `Password's must be at least 5 characters!` })
+    } else if (e.target.name === 'password') {
+      if (e.target.value.length < 6) {
+        setPasswordError(`Password's must be at least 5 characters!`)
       } else {
-        setErrorMessage({ password: `` })
+        setPasswordError(``)
       }
     }
 
@@ -50,6 +53,11 @@ export default function Example() {
       Auth.login(data.addUser.token);
     } catch (e) {
       console.error(e);
+
+      setAlert(true)
+      setTimeout(() => {
+        setAlert('')
+      }, 9000)
     }
   };
   return (
@@ -57,16 +65,16 @@ export default function Example() {
       <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
         <div className="mx-auto w-full max-w-sm lg:w-96">
           <div>
-
+            {signUpError && createError()}
             <h2 className="mt-6 text-3xl font-extrabold text-gray-100">Create an account</h2>
-
+            {error && <div className='py-3 text-red-500'>{error.message}</div>}
 
             <div className="mt-6">
               <form onSubmit={handleFormSubmit}>
                 <div>
                   <label htmlFor="username" className="block text-sm font-medium text-gray-700">
                     User Name
-                    {errorMessage.username && (<div className='py-3 text-red-500'>{errorMessage.username}</div>)}
+
                   </label>
                   <div className="mt-1">
                     <input
@@ -80,12 +88,13 @@ export default function Example() {
                       className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     />
                   </div>
+                  {userError && (<div className='py-3 text-red-500'>{userError}</div>)}
                 </div>
 
                 <div>
                   <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                     Email
-                    {errorMessage.email && (<div className='py-3 text-red-500'>{errorMessage.email}</div>)}
+
                   </label>
                   <div className="mt-1">
                     <input
@@ -99,12 +108,13 @@ export default function Example() {
                       className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     />
                   </div>
+                  {emailError && (<div className='py-3 text-red-500'>{emailError}</div>)}
                 </div>
 
                 <div className="space-y-1">
                   <label htmlFor="password" className="block text-sm font-medium text-gray-700">
                     Password
-                    {errorMessage.password && (<div className='py-3 text-red-500'>{errorMessage.password}</div>)}
+
                   </label>
                   <div className="mt-1">
                     <input
@@ -118,6 +128,7 @@ export default function Example() {
                       className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     />
                   </div>
+                  {passwordError && (<div className='py-3 text-red-500'>{passwordError}</div>)}
                 </div>
 
 
