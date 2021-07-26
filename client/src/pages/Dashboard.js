936c0b99f278944fe029907dcff2cf7a5e76a715
Landon Wilson <77355267+Landonwilson1@users.noreply.github.com>
2021-07-26 15:54:46 -0400

    discord layout

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -136,15 +136,15 @@ export default function Example() {
                     </div>
                 </div> */}
 
-                <main className="flex-1 relative overflow-y-auto focus:outline-none">
-                    <div className="py-6">
-                        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-                            <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+                <main className="flex flex-col min-h-screen">
+                    <div className="bg-gray-600 flex">
+                        <div className="bg-gray-700 w-56 h-screen flex-none">
+                            <h1 className="text-2xl font-semibold text-gray-900 mx-14 my-5">Proximo</h1>
                         </div>
                         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                             {/* Replace with your content */}
-                            <div className="py-4">
-                                <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+                            <div className="py-4 px-0">
+                                <div className=" bg bg-gray-500 h-96" />
                             </div>
                             {/* /End replace */}
                         </div>
