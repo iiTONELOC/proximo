c5c1c46c1b375e799ebda5b50da92c962706462d
ItsEvdok <semionaevdokimov@icloud.com>
2021-07-26 16:31:58 -0400

    testing

--- client/src/components/UserList/index.js
+++ client/src/components/UserList/index.js
@@ -31,10 +31,10 @@ const UserList = () => {
         }
     ];
 
-    console.log(data.me);
+    console.log(socket.connected);
 
     if (socket) {
-        if(data?.me){
+        if(data.me){
             users.push({
                 socketId: socket.id,
                 username: data.me.username,
@@ -49,7 +49,7 @@ const UserList = () => {
         return <div>
             <ul class="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
                 <li class="col-span-1 flex shadow-sm rounded-md">
-                    <div class="flex-shrink-0 flex items-center justify-center w-16 bg-pink-600 text-white text-sm font-medium rounded-l-md">
+                    <div class="flex-shrink-0 flex items-center justify-center w-16 bg-blue-600 text-white text-sm font-medium rounded-l-md">
                         GA
                     </div>
                     <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
