97aee5ae65616991978d3e2cf2b4f1b61e977070
Otis Singfield <singfieldotis@gmail.com>
2021-07-28 18:21:28 -0400

    style friend list

--- client/src/components/FriendList/index.js
+++ client/src/components/FriendList/index.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 const FriendList = ({ friendCount, username, friends }) => {
     if (!friends || !friends.length) {
-        return <p className="bg-dark text-light p-3">{username}, make some friends!</p>;
+        return <p className="bg-gray-600 text-gray-200 p-3">{username}, make some friends!</p>;
     }
 
     return (
@@ -14,7 +14,7 @@ const FriendList = ({ friendCount, username, friends }) => {
                 {username}'s {friendCount} {friendCount === 1 ? 'friend' : 'friends'}
             </h5>
             {friends.map(friend => (
-                <button className="btn w-100 display-block mb-2" key={friend._id}>
+                <button className="btn bg-gray-600 text-gray-200 w-100 display-block mb-2" key={friend._id}>
                     <Link to={`/profile/${friend.username}`}>{friend.username}</Link>
                 </button>
             ))}
