2008c0e77a4fe01d58479b3924ee9863a1317ea5
Otis Singfield <singfieldotis@gmail.com>
2021-07-28 18:53:55 -0400

    style commit

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -89,7 +89,7 @@ const Login = (props) => {
                   <div>
                     <button
                       type="submit"
-                      className="w-full mt-5 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      className="w-full mt-5 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                       Sign in
                     </button>
