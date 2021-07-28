e2c339d59ce2bf5e7aadec5e41631c523b9a8c3b
Otis Singfield <singfieldotis@gmail.com>
2021-07-28 18:15:49 -0400

    header styles

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -23,7 +23,7 @@ const logout = event => {
 };
 export default function Example() {
   return (
-    <header className="bg-indigo-600">
+    <header className="bg-gray-600">
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
         <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
           <div className="flex items-center">
@@ -32,7 +32,7 @@ export default function Example() {
               <img
                 // INSERT ICON HERE
                 className="h-10 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
+                src="https://img.icons8.com/color/48/000000/p.png"
                 alt=""
               />
             </Link>
@@ -43,13 +43,13 @@ export default function Example() {
           <div className="ml-10 space-x-4">
             {!Auth.loggedIn() ? <> <Link
               to="/login"
-              className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
+              className="inline-block bg-gray-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
             >
               Sign in
             </Link>
               <Link
                 to="/signup"
-                className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50"
+                className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-gray-600 hover:bg-indigo-50"
               >
                 Sign up
               </Link> </> : <><a
